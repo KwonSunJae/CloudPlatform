be832c8c7a4100c6656469460d195f6a671f9187
KwonSunJae <ksun4131@gmail.com>
2023-08-06 23:52:54 +0900

    feat : init the project

---
+++ Client/main/src/routes/home/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+
+
+export default function Home() {
+  
+  return (
+    
+    <div>
+      <h1>Home</h1>
+    </div>
+    
+  );
+}
