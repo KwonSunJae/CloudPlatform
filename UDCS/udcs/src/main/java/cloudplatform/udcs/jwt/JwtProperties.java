9fd85d8818e54718f34bd8cbf3a7c7ecf279f71c
ehdtndla123 <ehdtndla123@naver.com>
2024-01-22 12:56:54 +0900

    feat : add a jwt

---
+++ UDCS/udcs/src/main/java/cloudplatform/udcs/jwt/JwtProperties.java
@@ -0,0 +1,17 @@
+package cloudplatform.udcs.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+	private String issuer;
+	private String clientSecret;
+	private int tokenExpire;
+	private int refreshTokenExpire;
+}
