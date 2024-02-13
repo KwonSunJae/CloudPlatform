04c4a1db0b8911a750ce921895f7e9825898b332
ehdtndla123 <ehdtndla123@naver.com>
2024-02-13 15:59:57 +0900

    fix : 스크립트 파일 경로 재설정

--- UDCS/setup/setup.sh
+++ UDCS/setup/setup.sh
@@ -4,6 +4,8 @@ sudo apt install -y mysql-server
 
 sudo apt install -y openjdk-17-jdk
 
+#sudo apt install -y gradle
+
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
--- UDCS/start.sh
+++ UDCS/start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-BASE_PATH=/home/ubuntu/UDCS
-BUILD_PATH=$(ls $BASE_PATH/udcs/build/libs/*.jar)
+BASE_PATH=`pwd -P`
+BUILD_PATH=$(ls $BASE_PATH/udcs/build/libs/*.jar 2>/dev/null)
+
+
+#if [ -z "$BUILD_PATH" ]; then
+#    echo "JAR 파일이 존재하지 않습니다. 빌드를 시작합니다..."
+#    sudo $BASE_PATH/udcs/gradlew bootJar
+#    BUILD_PATH=$(ls $BASE_PATH/udcs/build/libs/*.jar)
+#fi
+
+
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
 echo "> build 파일 복사"
-DEPLOY_PATH=$BASE_PATH/setup/
+DEPLOY_PATH=$BASE_PATH/setup
 sudo cp $BUILD_PATH $DEPLOY_PATH
 
 
@@ -19,29 +28,30 @@ then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
-  kill -15 $IDLE_PID
+  sudo kill -15 $IDLE_PID
   sleep 5
 fi
 
 if ! nc -z localhost 5601; then
     echo "ELK is not running on port 5601."
-    
+
     cd setup
 
-    sudo docker-compose down
+    sudo docker-compose -f $DEPLOY_PATH/docker-compose.yml down
     sleep 5
-    
-    sudo docker compose up setup --force-recreate
+
+    sudo docker-compose -f $DEPLOY_PATH/docker-compose.yml up setup --force-recreate
     sleep 10
-    
-    sudo docker-compose up -d
+
+    sudo docker-compose -f $DEPLOY_PATH/docker-compose.yml up -d
     sleep 20
 else
-    echo "Running."
+    echo "ELK is Running."
 fi
 
 if ! nc -z localhost 8080; then
   echo "> Spring Application을 실행합니다."
+  cd setup
   sudo nohup java -jar $DEPLOY_PATH/*.jar &
 fi
 
