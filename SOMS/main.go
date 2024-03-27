b286e2d2862a0866c476a5b06099b3eea65b6556
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-03-27 23:25:09 +0900

    fix : Host IP

--- SOMS/main.go
+++ SOMS/main.go
@@ -28,7 +28,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:3000
+// @host 117.16.136,172:3000
 // @BasePath /
 func main() {
 	r := mux.NewRouter()
