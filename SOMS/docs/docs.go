bf7ddb4556caf68c4ebde10d0571467a749da6bb
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-03-27 23:25:09 +0900

    fix : Host IP swagger in soms
    fix : swagger in soms
    
    typo: fix typo
    
    typo : edit typo in swagger

--- SOMS/docs/docs.go
+++ SOMS/docs/docs.go
@@ -836,7 +836,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "117.16.136.172:3000",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "SOMS API",
