17094fe2a63524f26f7df17968ff33e367549cd0
ehdtndla123 <ehdtndla123@naver.com>
2024-03-28 19:58:31 +0900

    feat : Controller 부분 로그인 및 회원가입 추가, 유저 IP정보를 가져오기 위해 getRemoteAddr() 추가

--- UDCS/udcs/src/main/java/cloudplatform/udcs/controller/MemberController.java
+++ UDCS/udcs/src/main/java/cloudplatform/udcs/controller/MemberController.java
@@ -1,13 +1,18 @@
 package cloudplatform.udcs.controller;
 
+import cloudplatform.udcs.domain.Response;
 import cloudplatform.udcs.domain.TokenResponse;
+import cloudplatform.udcs.dto.RequestDto;
 import cloudplatform.udcs.service.MemberService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.PermitAll;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static cloudplatform.udcs.util.AuthenticationUtil.getMemberId;
 
 @Slf4j
 @RestController
@@ -19,11 +24,20 @@ public class MemberController {
     private final MemberService memberService;
 
     @PermitAll
-    @GetMapping("/test")
-    public ResponseEntity<TokenResponse> testSignup(
-            @RequestParam String ssid,
-            @RequestParam String userId
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(
+            @RequestBody RequestDto requestDto,
+            HttpServletRequest request
     ){
-        return ResponseEntity.ok(memberService.testSignup(ssid, userId));
+        return ResponseEntity.ok(memberService.login(requestDto,request.getRemoteAddr()));
+    }
+
+    @PermitAll
+    @PostMapping("/signup")
+    public ResponseEntity<Response> signUp(
+            @RequestBody RequestDto requestDto,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.ok(memberService.signUp(requestDto,request.getRemoteAddr()));
     }
 }
