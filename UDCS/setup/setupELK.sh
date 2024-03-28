01d8b4a1016699e5350d382a5f325cab2e67eb8d
ehdtndla123 <ehdtndla123@naver.com>
2024-03-28 20:01:13 +0900

    feat : ELK 설치준비 스크립트 추가

---
+++ UDCS/setup/setupELK.sh
@@ -0,0 +1,16 @@
+echo "시스템 패키지 업데이트 중..."
+sudo apt update
+echo "시스템 패키지 업데이트 완료."
+
+echo "Docker 및 Docker Compose 설치 중..."
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+echo "Docker 및 Docker Compose 설치 완료."
