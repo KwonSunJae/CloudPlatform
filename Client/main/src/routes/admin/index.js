6d0dc0c315b68d94d3ab1c8bf246514f47b490a9
Dong <59868624+ehdtndla123@users.noreply.github.com>
2024-06-11 20:25:07 +0900

    feat : add admin page - user approve, get user info, user delete (#68)

--- Client/main/src/routes/admin/index.js
+++ Client/main/src/routes/admin/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import NavigationBar from "../../components/navigationbar";
 import Footer from "../../components/footer";
-import Admin from "../../components/admin";
+import Admin from "../../components/admin/user";
+import AdminNavbar from "../../components/admin/navbar";
 
 
 
@@ -12,6 +13,7 @@ export default function AdminPage() {
     
     <div>
     <NavigationBar />
+    <AdminNavbar />
     <Admin />
     <Footer />
     </div>
