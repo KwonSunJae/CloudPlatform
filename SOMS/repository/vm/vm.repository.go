9294530dca165af76db533cd4305b9d7670dac8a
KwonSunJae <ksun4131@gmail.com>
2024-06-12 17:12:24 +0900

    fix : error point in vm repository

--- SOMS/repository/vm/vm.repository.go
+++ SOMS/repository/vm/vm.repository.go
@@ -57,7 +57,7 @@ func (r *VmRepository) InsertVm(n VmDto) (sql.Result, error) {
     (id, name, flavorID, externalIP, internalIP,selectedOS, unionmountImage, keypair,selectedSecuritygroup ,uuid,status)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `
-	result, err := r.DB.Exec(query, id.String(), n.Name, n.FlavorID, n.ExternalIP, n.InternalIP, n.SelectedOS, n.UnionmountImage, n.Keypair, n.SelectedSecuritygroup, n.Status, n.UUID)
+	result, err := r.DB.Exec(query, id.String(), n.Name, n.FlavorID, n.ExternalIP, n.InternalIP, n.SelectedOS, n.UnionmountImage, n.Keypair, n.SelectedSecuritygroup, n.UUID, n.Status)
 
 	if err != nil {
 		return nil, err
