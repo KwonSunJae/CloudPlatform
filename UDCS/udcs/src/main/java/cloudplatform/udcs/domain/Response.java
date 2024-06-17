add92be1dca696865d2f22584807f05e0f2293ad
ehdtndla123 <ehdtndla123@naver.com>
2024-06-17 20:22:32 +0900

    fix : data type String -> Object

--- UDCS/udcs/src/main/java/cloudplatform/udcs/domain/Response.java
+++ UDCS/udcs/src/main/java/cloudplatform/udcs/domain/Response.java
@@ -1,16 +1,33 @@
 package cloudplatform.udcs.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Response {
     @JsonProperty("data")
-    private String data;
+    private Object data;
 
     @JsonProperty("status")
     private int status;
 
     @JsonProperty("error")
     private Object error;
+
+    @JsonIgnore
+    public String getDataAsString() {
+        if (data instanceof String) {
+            return (String) data;
+        }
+        return null;
+    }
+
+    @JsonIgnore
+    public String[] getDataAsStringArray() {
+        if (data instanceof String[]) {
+            return (String[]) data;
+        }
+        return null;
+    }
 }
