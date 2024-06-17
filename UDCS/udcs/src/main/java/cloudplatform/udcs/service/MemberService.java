a265de35c902a03d4cd1acbe2bb5997993d2cf1f
ehdtndla123 <ehdtndla123@naver.com>
2024-06-17 20:23:35 +0900

    feat : add reissue function

--- UDCS/udcs/src/main/java/cloudplatform/udcs/service/MemberService.java
+++ UDCS/udcs/src/main/java/cloudplatform/udcs/service/MemberService.java
@@ -3,6 +3,7 @@ package cloudplatform.udcs.service;
 import cloudplatform.udcs.domain.*;
 import cloudplatform.udcs.dto.RequestDto;
 import cloudplatform.udcs.exception.AuthException;
+import cloudplatform.udcs.exception.ExistMemberException;
 import cloudplatform.udcs.jwt.Jwt;
 import cloudplatform.udcs.repository.MemberRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
@@ -45,7 +47,7 @@ public class MemberService {
         String transactionId = UUID.randomUUID().toString();
 
         Response response = apiServer(requestDto, transactionId, remoteAddr);
-        String ssid = response.getData();
+        String uuid = response.getDataAsString();
         String userId="";
 
         try {
@@ -55,15 +57,16 @@ public class MemberService {
             e.printStackTrace();
         }
 
-        if (!memberRepository.existsBySsidAndUserId(ssid, userId)) {
+        if (!memberRepository.existsByUuidAndUserId(uuid, userId)) {
             memberRepository.save(Member.builder()
                     .userId(userId)
-                    .ssid(ssid)
+                    .uuid(uuid)
                     .memberAuthority(MemberAuthority.USER)
                     .build());
         }
 
         logEnd(transactionId, UUID.randomUUID().toString());
+        if(response.getStatus()==200) response.setData("OK");
         return response;
     }
 
@@ -74,7 +77,7 @@ public class MemberService {
         String transactionId = UUID.randomUUID().toString();
 
         Response response = apiServer(requestDto, transactionId, remoteAddr);
-        String ssid = response.getData();
+        String uuid = response.getDataAsString();
         String userId="";
 
         try {
@@ -85,7 +88,7 @@ public class MemberService {
         }
 
         logEnd(transactionId, UUID.randomUUID().toString());
-        Member member = memberRepository.findBySsidAndUserId(ssid, userId)
+        Member member = memberRepository.findByUuidAndUserId(uuid, userId)
                 .orElseThrow(() -> new AuthException("해당 회원을 찾을 수 없습니다."));
 
         return publishToken(member);
@@ -103,6 +106,32 @@ public class MemberService {
         );
 
         member.setRefreshToken(tokenResponse.refreshToken());
+
+        return tokenResponse;
+    }
+
+    @Transactional
+    public TokenResponse reissue(String refreshToken, String remoteAddr) {
+        var member = memberRepository.findByRefreshToken(refreshToken);
+
+        if (member.isPresent() == false) {
+            throw new AccessDeniedException("refresh token 이 만료되었습니다.");
+        }
+
+        Long memberId;
+        String[] roles;
+
+        try {
+            Jwt.Claims claims = jwt.verify(member.get().getRefreshToken());
+            memberId = claims.getMemberId();
+            roles = claims.getRoles();
+        } catch (Exception e) {
+            log.warn("Jwt 처리중 문제가 발생하였습니다 : {}", e.getMessage());
+            throw e;
+        }
+        TokenResponse tokenResponse = jwt.generateAllToken(Jwt.Claims.from(memberId, roles));
+
+        member.get().setRefreshToken(tokenResponse.refreshToken());
 
         return tokenResponse;
     }
@@ -133,13 +162,21 @@ public class MemberService {
                 }
 
                 ResponseEntity<Response> responseEntity = restTemplate.exchange(SOMS_URL + requestDto.getDest(), HttpMethod.valueOf(requestDto.getMethod()), entity, Response.class);
-
+                log.info(""+responseEntity.getBody().getStatus());
                 if (responseEntity.getBody().getStatus() == 500) {
                     currentAttempt++;
                     continue;
                 }
-                if (responseEntity.getBody().getStatus() == 400) {
+                if (responseEntity.getBody().getStatus() == 401) {
                     throw new AuthException("올바르지 않은 로그인 정보입니다.");
+                }
+
+                if (responseEntity.getBody().getStatus() == 400) {
+                    throw new AuthException("올바르지 않은 요청입니다.");
+                }
+
+                if (responseEntity.getBody().getStatus() == 409) {
+                    throw new ExistMemberException("이미 존재하는 아이디입니다.");
                 }
 
                 response = responseEntity.getBody();
