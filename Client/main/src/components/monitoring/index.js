1ae7ed7c71ee3bf84dc78b1c4383649b1b59c028
ehdtndla123 <ehdtndla123@naver.com>
2024-06-20 18:14:12 +0900

    feat : add monitoring link

--- Client/main/src/components/monitoring/index.js
+++ Client/main/src/components/monitoring/index.js
@@ -7,13 +7,18 @@ const ShowMonitoring = () => {
   return (
     <div className="monitoring">    
       <div className="monitoring-webview">
+        <a href ="http://117.16.137.217:5601/app/dashboards#/view/34e8b060-2d90-11ef-999c-590a08d8265c?embed=true&_g=(refreshInterval:(pause:!t,value:60000),time:(from:now-15m,to:now))&_a=()" target="_blank">
+          <Button variant="primary">View Full Screen</Button>
+        </a>
         <iframe 
           src="http://117.16.137.217:5601/app/home#/" 
           title="Web Monitoring"
           width="100%" 
           height="600px" 
           style={{ border: 'none' }}
-        ></iframe>
+        >
+        </iframe>
+
       </div>
   
     </div>
