98315f7a6f3b32bba6cba36708bfa2e1559df353
KwonSunJae <ksun4131@gmail.com>
2024-07-15 14:36:55 +0900

    init : test framework

--- SOMS/go.mod
+++ SOMS/go.mod
@@ -6,7 +6,13 @@ toolchain go1.22.1
 
 require github.com/swaggo/http-swagger v1.3.4
 
-require github.com/gophercloud/gophercloud v1.12.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gophercloud/gophercloud v1.12.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
---
+++ SOMS/service/user/user.service_test.go
@@ -0,0 +1,29 @@
+package user_test
+
+import (
+	"testing"
+)
+
+func TestGetAllUser(t *testing.T) {
+	t.Log("TestGetAllUser")
+}
+
+func TestGetOneUserByUUID(t *testing.T) {
+	t.Log("TestGetOneUserByUUID")
+}
+
+func TestCreateUser(t *testing.T) {
+	t.Log("TestCreateUser")
+}
+
+func TestApproveUser(t *testing.T) {
+	t.Log("TestApproveUser")
+}
+
+func TestDeleteOneUser(t *testing.T) {
+	t.Log("TestDeleteOneUser")
+}
+
+func TestUpdateUser(t *testing.T) {
+	t.Log("TestUpdateUser")
+}
