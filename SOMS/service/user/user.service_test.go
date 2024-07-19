b8792357a7ba14b2ef172a482128f0f43e3bc9c7
KwonSunJae <ksun4131@gmail.com>
2024-07-20 05:07:25 +0900

    feat : User Domain Unit Test

--- SOMS/service/user/user.service_test.go
+++ SOMS/service/user/user.service_test.go
@@ -2,6 +2,7 @@ package user_test
 
 import (
 	"errors"
+	"fmt"
 	mocks "soms/mocks/user"
 	model "soms/model/user"
 	"soms/service/user"
@@ -31,6 +32,7 @@ func TestGetAllUser(t *testing.T) {
 		}, nil)
 
 		raws, err := userService.GetAllUser()
+		fmt.Println(raws)
 		assert.NotNil(t, raws)
 		assert.Nil(t, err)
 	})
@@ -46,20 +48,91 @@ func TestGetAllUser(t *testing.T) {
 
 func TestGetOneUserByUUID(t *testing.T) {
 	t.Log("TestGetOneUserByUUID")
+
+	t.Run("TestGetOneUserByUUIDSuccess", func(t *testing.T) {
+		mockUserRepository := new(mocks.UserRepository)
+		userService := user.NewUserService(&user.USConfig{Repository: mockUserRepository})
+
+		mockUserRepository.On("GetOneUserByUUID", "test").Return(&model.UserRaw{
+			Id:          "test",
+			Name:        "test",
+			UserID:      "test",
+			EncryptedPW: "test",
+			Role:        "test",
+			Spot:        "test",
+			Priority:    "test",
+		}, nil)
+
+		raw, err := userService.GetOneUserByUUID("test")
+		fmt.Println(raw)
+		assert.NotNil(t, raw)
+		assert.Nil(t, err)
+	})
+
+	t.Run("TestGetOneUserByUUIDError", func(t *testing.T) {
+		mockUserRepository := new(mocks.UserRepository)
+		mockUserRepository.On("GetOneUserByUUID", "test").Return(nil, errors.New("error"))
+		userService := user.UserService{Repository: mockUserRepository}
+		raw, err := userService.GetOneUserByUUID("test")
+		assert.Nil(t, raw)
+		assert.NotNil(t, err)
+	})
+
 }
 
 func TestCreateUser(t *testing.T) {
 	t.Log("TestCreateUser")
+	//logic 수정예정
 }
 
 func TestApproveUser(t *testing.T) {
 	t.Log("TestApproveUser")
+	//logic 수정예정
 }
 
 func TestDeleteOneUser(t *testing.T) {
 	t.Log("TestDeleteOneUser")
+	//logic 수정예정
 }
 
 func TestUpdateUser(t *testing.T) {
 	t.Log("TestUpdateUser")
+	//logic 수정예정
+}
+
+func TestIsUserIDValidate(t *testing.T) {
+	t.Log("TestIsUserIDExit")
+	t.Run("TestUserIDValidatetSuccess", func(t *testing.T) {
+		mockUserRepository := new(mocks.UserRepository)
+		userService := user.NewUserService(&user.USConfig{Repository: mockUserRepository})
+
+		mockUserRepository.On("IsUserIDExit", "test").Return(true, nil)
+
+		isExit, err := userService.UserIDValidate("test")
+		assert.Equal(t, true, isExit)
+		assert.Nil(t, err)
+	})
+
+	t.Run("TestUserIDValidatetError", func(t *testing.T) {
+		mockUserRepository := new(mocks.UserRepository)
+		mockUserRepository.On("IsUserIDExit", "test").Return(false, errors.New("error"))
+		userService := user.UserService{Repository: mockUserRepository}
+		isExit, err := userService.UserIDValidate("test")
+		assert.Equal(t, false, isExit)
+		assert.NotNil(t, err)
+	})
+
+}
+
+func TestUserLogin(t *testing.T) {
+	t.Log("TestUserLogin")
+	t.Run("TestUserLoginSuccess", func(t *testing.T) {
+		mockUserRepository := new(mocks.UserRepository)
+		userService := user.NewUserService(&user.USConfig{Repository: mockUserRepository})
+
+		mockUserRepository.On("GetEncryptedPW", "test").Return("test", nil)
+		isLogin, err := userService.UserLogin("test", "test")
+		assert.Equal(t, true, isLogin)
+		assert.Nil(t, err)
+	})
 }
